f15a05b84ffe1d6f67a67a122d9bcc57fbab2612
a58982284 <a58982284@163.com>
2018-09-18 22:46:06 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,108 @@
+from __future__ import unicode_literals
+import os
+from flask import (Flask, render_template, redirect, url_for, request, flash)
+from flask_bootstrap import Bootstrap
+from flask_login import login_required, login_user, logout_user, current_user
+
+from forms import TodoListForm, LoginForm
+from ext import db, login_manager
+from models import TodoList, User
+
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = 'key value'
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.secret_key = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')  # 程序使用的数据库URL必须保存到SQLALCHEMY_DATABASE_URI变量
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  # 每次请求结束都会自动提交事务
+
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@app.route('/', methods=['GET', 'POST'])
+@login_required
+def show_todo_list():
+    form = TodoListForm()
+    if request.method == 'GET':
+        todolists = TodoList.query.all()
+        return render_template('index.html', todolists=todolists, form=form)
+    else:
+        if form.validate_on_submit():
+            todolist = TodoList(current_user.id, form.title.data, form.status.data)
+            db.session.add(todolist)
+            db.session.commit()
+            flash('You have add a new todo list')
+        else:
+            flash(form.errors)
+        return redirect(url_for('show_todo_list'))
+
+
+@app.route('/delete/<int:id>')
+@login_required
+def delete_todo_list(id):
+    todolist = TodoList.query.filter_by(id=id).first_or_404()
+    db.session.delete(todolist)
+    db.session.commit()
+    flash('You have delete a todo list')
+    return redirect(url_for('show_todo_list'))
+
+
+@app.route('/change/<int:id>', methods=['GET', 'POST'])
+@login_required
+def change_todo_list(id):
+    if request.method == 'GET':
+        todolist = TodoList.query.filter_by(id=id).first_or_404()
+        form = TodoListForm()
+        form.title.data = todolist.title
+        form.status.data = str(todolist.status)
+        return render_template('modify.html', form=form)
+    else:
+        form = TodoListForm()
+    if form.validate_on_submit():
+        todolist = TodoList.query.filter_by(id=id).first_or_404()
+        todolist.title = form.title.data
+        todolist.status = form.status.data
+        db.session.commit()
+        flash('You have modify a todolist')
+    else:
+        flash(form.errors)
+    return redirect(url_for('show_todo_list'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username'])
+        if user:
+            logout_user(user)
+            flash('you have logged in!')
+            return redirect(url_for('show_todo_list'))
+        else:
+            flash('Invalid username or password')
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('you have logout!')
+    return redirect(url_for('login'))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=int(user_id)).first()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
